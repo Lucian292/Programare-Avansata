565e079576f74a21fd55854f12dd00cae0cf4694
Lucian <lucian_munteanu@mail.ru>
2023-03-21 16:56:52 +0200

    update of sorting algorithm

--- lab 3/Homework/Main.java
+++ lab 3/Homework/Main.java
@@ -1,36 +1,41 @@
 import java.util.*;
 import java.time.LocalDate;
-    public class Main {
-        public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
 
-            // create some people
-            Person alice = new Person("Alice", new Date(1980, 1, 1));
-            Person bob = new Person("Bob", new Date(1990, 1, 1));
-            Programmer charlie = new Programmer("Charlie", new Date(1995, 1, 1), "Java");
-            Designer diana = new Designer("Diana", new Date(1985, 1, 1), "User Experience");
+        // create some people
+        Person alice = new Person("Alice", new Date(1980, 1, 1));
+        Person bob = new Person("Bob", new Date(1990, 1, 1));
+        Programmer charlie = new Programmer("Charlie", new Date(1995, 1, 1), "Java");
+        Designer diana = new Designer("Diana", new Date(1985, 1, 1), "User Experience");
 
-            // create some companies
-            Company acme = new Company("Acme Inc.", "Manufacturing");
-            Company globex = new Company("Globex Corp.", "Technology");
+        // create some companies
+        Company camline = new Company("CamLine", "Manufacturing");
+        Company vitesco = new Company("Vitesco", "Technology");
 
-            // add some employees to the companies
-            acme.addEmployee(alice, "Manager");
-            acme.addEmployee(bob, "Engineer");
-            globex.addEmployee(charlie, "Developer");
-            globex.addEmployee(diana, "Designer");
+        // create the network and add the nodes
+        Network network = new Network();
+        network.addNode(alice);
+        network.addNode(bob);
+        network.addNode(charlie);
+        network.addNode(diana);
+        network.addNode(camline);
+        network.addNode(vitesco);
 
-            // create the network and add the nodes
-            Network network = new Network();
-            network.addNode(alice);
-            network.addNode(bob);
-            network.addNode(charlie);
-            network.addNode(diana);
-            network.addNode(acme);
-            network.addNode(globex);
+        //adaugam relatii intre persoane
+        alice.addRelationship(bob, "frind");
+        alice.addRelationship(charlie, "husbands");
+        diana.addRelationship(charlie, "coworkers");
 
-            // compute and print the importance of each node in the network
-            network.computeImportance();
-            network.printNetwork();
+        // add some employees to the companies
+        camline.addEmployee(alice, "Manager");
+        camline.addEmployee(bob, "Engineer");
+        vitesco.addEmployee(charlie, "Developer");
+        vitesco.addEmployee(diana, "Designer");
 
-        }
+        // compute and print the importance of each node in the network
+        network.computeImportance();
+        network.printNetwork();
+
     }
+}
--- lab 3/Homework/Network.java
+++ lab 3/Homework/Network.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 import java.util.List;
 public class Network {
 
+    // create a map to store each node's importance (i.e., number of connections to other nodes)
+    Map<Node, Integer> importance = new HashMap<Node, Integer>();
+    Map<Node, Integer> sortedImportance = new LinkedHashMap<>();
     private List<Node> nodes;
 
     public Network() {
@@ -16,8 +19,7 @@ public class Network {
 
     // computes the importance of each node in the network and sorts them in descending order
     public void computeImportance() {
-        // create a map to store each node's importance (i.e., number of connections to other nodes)
-        Map<Node, Integer> importance = new HashMap<Node, Integer>();
+
 
         // iterate over each node in the network
         for (Node node : nodes) {
@@ -33,31 +35,26 @@ public class Network {
             // add the node and its importance to the map
             importance.put(node, connections);
         }
-
-        // sort the nodes in descending order of importance (i.e., number of connections)
-        Collections.sort(nodes, new Comparator<Node>() {
-            public int compare(Node n1, Node n2) {
-                return importance.get(n2) - importance.get(n1);
+        // sort the map in descending order of importance
+        List<Map.Entry<Node, Integer>> entries = new ArrayList<>(importance.entrySet());
+        Collections.sort(entries, new Comparator<Map.Entry<Node, Integer>>() {
+            public int compare(Map.Entry<Node, Integer> e1, Map.Entry<Node, Integer> e2) {
+                return e2.getValue().compareTo(e1.getValue());
             }
         });
+
+        // create a new LinkedHashMap to store the sorted entries
+        for (Map.Entry<Node, Integer> entry : entries) {
+            sortedImportance.put(entry.getKey(), entry.getValue());
+        }
     }
 
     // prints the network, with nodes ordered by importance (i.e., number of connections to other nodes)
     public void printNetwork() {
-        for (Node node : nodes) {
-            System.out.print(node.getClass().getSimpleName() + ": " + node.getName());
-            Map<Node, String> relationships = node.getRelationships();
-            if (!relationships.isEmpty()) {
-                System.out.print(" (");
-                for (Map.Entry<Node, String> entry : relationships.entrySet()) {
-                    Node otherNode = entry.getKey();
-                    String relationship = entry.getValue();
-                    System.out.print(otherNode.getName() + " - " + relationship + ", ");
-                }
-                System.out.print("\b\b)"); // remove trailing comma and space
-            }
-            System.out.println();
+        for (Map.Entry<Node, Integer> entry : sortedImportance.entrySet()) {
+            Node node = entry.getKey();
+            int importanceValue = entry.getValue();
+            System.out.println("Node: " + node.getName() + ", Importance: " + importanceValue);
         }
     }
-
 }
